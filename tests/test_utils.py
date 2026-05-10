1331e72cffd0671975982e742d5a803ac92768a7
wididestrianda01 <97785006+wididestrianda01@users.noreply.github.com>
2026-05-10 17:10:01 +0200

    style: rename test functions to match spec

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,19 +10,19 @@ from src.utils import compute_returns
 class TestComputeReturns:
     """Tests for the compute_returns function."""
 
-    def test_log_returns(self):
+    def test_compute_returns_log(self):
         prices = pd.Series([100.0, 101.0, 99.0])
         result = compute_returns(prices, method="log")
         expected = np.array([np.log(101 / 100), np.log(99 / 101)])
         assert np.allclose(result, expected)
 
-    def test_simple_returns(self):
+    def test_compute_returns_simple(self):
         prices = pd.Series([100.0, 102.0, 98.0])
         result = compute_returns(prices, method="simple")
         expected = np.array([0.02, -0.0392156862745098])
         assert np.allclose(result, expected)
 
-    def test_constant_prices(self):
+    def test_compute_returns_constant_prices(self):
         prices = pd.Series([100.0, 100.0, 100.0])
         result = compute_returns(prices, method="log")
         assert np.allclose(result, [0.0, 0.0])
